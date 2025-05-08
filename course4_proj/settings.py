80266fa8d6dce195b637e2e271c6ee1503123594
Arttu Lehtovaara <arttu.lehtovaara@metropolia.fi>
2025-05-08 14:58:17 +0000

    Finish celery introduction

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -50,6 +50,7 @@ class Dev(Configuration):
       'django.contrib.staticfiles',
       'movies',
       'gh',
+      'django_celery_results',
   ]
 
   MIDDLEWARE = [
@@ -160,3 +161,6 @@ class Dev(Configuration):
   }
 
   OMDB_KEY = "abc123"
+
+  CELERY_RESULT_BACKEND = "django-db"
+  CELERY_BROKER_URL = "redis://localhost:6379/0"
